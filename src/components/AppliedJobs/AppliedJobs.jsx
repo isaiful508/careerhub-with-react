de232871c41768154240d6e5f65f5e67eea6f3ef
isaiful508 <isaiful.islam508@gmail.com>
2024-03-24 16:47:15 +0600

    half done

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,9 +1,65 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../../Utility/LocalStorage";
 
 
 const AppliedJobs = () => {
+    const jobs = useLoaderData();
+    const [appliedJobs, setAppliedJobs] = useState([]);
+    const [dispalayJobs, setDisplayJobs] = useState([]);
+
+    const handleJobsFilter = filter =>{
+        if(filter === 'all'){
+            setDisplayJobs(appliedJobs);
+        }else if (filter === "remote"){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+           
+        }else if (filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsiteJobs);
+        }
+    }
+
+    useEffect(() => {
+        const storeJobsIds = getStoredJobApplication();
+        if (jobs.length > 0) {
+            // const jobsApplied = jobs.filter(job => storeJobsId.includes(job.id) );
+
+            //another way
+            const jobsApplied = [];
+            for (const id of storeJobsIds) {
+                const job = jobs.find(job => job.id === id);
+                if (job) {
+                    jobsApplied.push(job);
+                }
+            }
+
+            setAppliedJobs(jobsApplied);
+            setDisplayJobs(jobsApplied);
+
+            // console.log(jobsApplied);
+        }
+    }, [jobs])
+
     return (
         <div>
-            <h2>I have applied jobs</h2>
+            <h2 className="text-2xl">I have applied jobs: {appliedJobs.length}</h2>
+            <details className="dropdown">
+                <summary className="m-1 btn">Fitler</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                    <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                    <li onClick={() => handleJobsFilter('onsite')}><a>Onsite</a></li>
+                </ul>
+            </details>
+            <ul>
+                {
+                    dispalayJobs.map(job => <li key={job.id} >
+                        <span>{job.job_title} {job.company_name} : {job.remote_or_onsite}</span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -4,15 +4,26 @@ import { SlCalender } from "react-icons/sl";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 import { CiLocationOn } from "react-icons/ci";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../Utility/LocalStorage";
 
 
 
 const JobDetails = () => {
     const jobs = useLoaderData();
-    const { id, job_description, job_responsibility,educational_requirements,experiences , contact_information,salary,job_title  } = useParams();
+    const { id, job_description, job_responsibility, educational_requirements, experiences, contact_information, salary, job_title } = useParams();
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
     console.log(id, job);
+
+    const handleApply = () =>{
+        saveJobApplication(idInt);
+        toast('You have applied successfully');
+    }
+ 
+
+
     return (
         <div>
             <h2>Jod Details of : {id}</h2>
@@ -32,24 +43,24 @@ const JobDetails = () => {
 
                     <h2 className="text-2xl font-semibold">Job Details</h2>
                     <hr />
-                     
-                     <h2> <CiDollar></CiDollar> Salary : {job.salary}</h2>
-                     <h2> <SlCalender></SlCalender> Job-Title: {job.job_title}</h2>
-                   
-                     <h2 className="text-2xl font-semibold">Contact Information: </h2>
-                     <h2>Phone: {job.contact_information.phone}</h2>
-                     <h2>Email: {job.contact_information.email}</h2>
-                     <h2>Address: {job.contact_information.address}</h2>
-                    
-                     
-                     <button className="btn btn-primary w-full">Apply Now</button>
+
+                    <h2> <CiDollar></CiDollar> Salary : {job.salary}</h2>
+                    <h2> <SlCalender></SlCalender> Job-Title: {job.job_title}</h2>
+
+                    <h2 className="text-2xl font-semibold">Contact Information: </h2>
+                    <h2>Phone: {job.contact_information.phone}</h2>
+                    <h2>Email: {job.contact_information.email}</h2>
+                    <h2>Address: {job.contact_information.address}</h2>
+
+
+                    <button onClick={handleApply} className="btn btn-primary w-full">Apply Now</button>
 
                 </div>
 
-               
+
 
             </div>
-
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
